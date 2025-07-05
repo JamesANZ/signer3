8cfdd7b2cba214f1062b147f69c171a60210b984
bitcoinwarrior1 <j.l.sangalli@gmail.com>
2025-07-05 10:31:03 +1000

    save

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/test'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  testMatch: ['**/?(*.)+(test).ts?(x)'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+};
+
+export default config; 
---
+++ test/basic.test.ts
@@ -0,0 +1,5 @@
+describe('Basic Jest Suite', () => {
+  it('should run a basic test', () => {
+    expect(true).toBe(true);
+  });
+}); 
